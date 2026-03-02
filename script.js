ae741eaaefd1763cbcefc1410d140c1541782a43
Aditya Singhal <poojaagg257@gmail.com>
2026-03-02 23:14:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// console.log("Hello, World! This is a simple JavaScript file.")  ;
+// document.write("Welcome to JavaScript programming!");
+var num=10;
+    console.log(num);
+    console.log(typeof(num));
+    num=false;
+    console.log(num);
+    console.log(typeof(num));
+    num="Sanjeev Soni";
+console.log(num);
+    console.log(typeof(num)); 
+    var sayHello=function(){
+    alert("Hello World");
+    }
+    var numberArray=[1,2,3,4,5];
+    var animals=new Array("dog","cat","lion","tiger");
+    var person= {
+        name: "Barack Hussein Obama II",
+        age: 62,
+        title: "44th President of the United States",
+    }
+    const sym1=Symbol(4)
+    console.log(sym1);
+    const sym2=Symbol(4)
+    console.log(sym2);
+    if(sym1===sym2)
+    {
+        console.log("Symbols are equal");
+    }
