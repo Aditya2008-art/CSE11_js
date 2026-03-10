0ff87b375317187100c7bed7c05c98dabaea150f
Aditya2008-art <poojaagg257@gmail.com>
2026-03-10 09:45:41 +0530

    New

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 // console.log("Hello, World! This is a simple JavaScript file.")  ;
 // document.write("Welcome to JavaScript programming!");
-var num=10;
-    console.log(num);
-    console.log(typeof(num));
-    num=false;
-    console.log(num);
-    console.log(typeof(num));
-    num="Sanjeev Soni";
-console.log(num);
-    console.log(typeof(num)); 
-    var sayHello=function(){
-    alert("Hello World");
-    }
-    var numberArray=[1,2,3,4,5];
-    var animals=new Array("dog","cat","lion","tiger");
-    var person= {
-        name: "Barack Hussein Obama II",
-        age: 62,
-        title: "44th President of the United States",
-    }
-    const sym1=Symbol(4)
-    console.log(sym1);
-    const sym2=Symbol(4)
-    console.log(sym2);
-    if(sym1===sym2)
-    {
-        console.log("Symbols are equal");
-    }
+// var num=10;
+//     console.log(num);
+//     console.log(typeof(num));
+//     num=false;
+//     console.log(num);
+//     console.log(typeof(num));
+//     num="Sanjeev Soni";
+// console.log(num);
+//     console.log(typeof(num)); 
+//     var sayHello=function(){
+//     alert("Hello World");
+//     }
+//     var numberArray=[1,2,3,4,5];
+//     var animals=new Array("dog","cat","lion","tiger");
+//     var person= {
+//         name: "Barack Hussein Obama II",
+//         age: 62,
+//         title: "44th President of the United States",
+//     }
+//     const sym1=Symbol(4)
+//     console.log(sym1);
+//     const sym2=Symbol(4)
+//     console.log(sym2);
+//     if(sym1===sym2)
+//     {
+//         console.log("Symbols are equal");
+//     }
+    var myInfo = new Array();
+        var myInfo1= Array();
+        console.log(myInfo,myInfo1);
+
+        let collection=[
+            {},[],true,"john", function() {},90098763193, undefined, null, new String("abc"),new Date()
+    ]
+        console.log(collection);
+        collection.teachername="John doe";
+        console.log(collection.length);
---
+++ script2.js
@@ -0,0 +1,10 @@
+ var myInfo = new Array();
+        var myInfo1= Array();
+        console.log(myInfo,myInfo1);
+
+        let collection=[
+            {},[],true,"john", function() {},90098763193, undefined, null, new String("abc"),new Date()
+    ]
+        console.log(collection);
+        collection.teachername="John doe";
+        console.log(collection.length);
