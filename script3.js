c83efa16138b38cf2683da16cce68d8c7720c312
Aditya2008-art <poojaagg257@gmail.com>
2026-03-17 10:01:12 +0530

    events

---
+++ script3.js
@@ -0,0 +1,31 @@
+function onClick()
+{
+    console.log("Clicked");
+    alert("Button Clicked");
+}
+function onMouseOver()
+{
+    console.log("onMouseOver");
+    alert("Mouse Over");
+}
+function onMouseOut()
+{
+    console.log("onMouseOut");
+    alert("Button Clicked");
+}
+function onMouseUp()
+{
+    console.log("onMouseUp");
+    alert("Button Clicked");
+
+}
+function onMouseDown()
+{
+    console.log("onMouseDown");
+    alert("Button Clicked");
+}
+function onMouseMove()
+{
+    console.log("onMouseMove");
+    alert("Button Clicked");
+}
