73e4b69e14a0f3dcfe805f8b03c94dd557921306
Aditya2008-art <poojaagg257@gmail.com>
2026-04-21 20:57:18 +0530

    companies.js

---
+++ companies.js
@@ -0,0 +1,8 @@
+let a=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+console.log(a);
+console.log(a.unshift());
+console.log(a);
+console.log(a.replace("Uber","Ola"));
+console.log(a);
+console.log(a.push("Amazon"));
+console.log(a);
