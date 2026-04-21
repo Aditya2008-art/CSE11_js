5b971f3a7992324129c2e242056393ced7d143c8
Aditya2008-art <poojaagg257@gmail.com>
2026-04-21 20:58:29 +0530

    discounted.js

---
+++ discounted.js
@@ -0,0 +1,6 @@
+let a=[250, 645, 300, 900, 50];
+for(let i=0;i<5;i++)
+{
+    console.log("Original Price of item", i+1, "is: ", a[i]);
+    console.log("Price after 10% discount is: ", (0.9*a[i]));
+}
