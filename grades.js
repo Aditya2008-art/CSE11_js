eb9f2c3615c25c9add10f74c1b29efcd14d9d151
Aditya2008-art <poojaagg257@gmail.com>
2026-04-21 21:04:35 +0530

    grades.js

---
+++ grades.js
@@ -0,0 +1,11 @@
+let a=prompt("Enter your marks:");
+if(a>=80&&a<=100)
+    console.log('A');
+else if(a>=70)
+    console.log('B');
+else if(a>=60)
+    console.log('C');
+else if(a>=50)
+    console.log('D');
+else
+    console.log('F');
